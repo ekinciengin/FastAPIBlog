6ecfc5b46ec77a1227b170a188f86fa8f23dd54a
ekinciengin <75222683+ekinciengin@users.noreply.github.com>
2021-12-24 17:33:14 +0000

    All changes

--- blog/main.py
+++ blog/main.py
@@ -5,10 +5,10 @@ from blog.database import engine
 from blog.routers import blog, user, authentication
 from mangum import Mangum
 
-stage = os.environ.get('STAGE',None)
+stage = os.environ.get('STAGE', None)
 openapi_prefix = f"/{stage}" if stage else "/"
 
-app = FastAPI(title="MyBlog", openapi_prefix=openapi_prefix) # Here is the magic
+app = FastAPI(title="MyBlog", openapi_prefix=openapi_prefix)  # Here is the magic
 
 models.Base.metadata.create_all(bind=engine)
 
